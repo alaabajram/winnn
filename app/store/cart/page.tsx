378e6d860828366940a64233a05ccd5282cfad00
alaabajram <alaabajram@gmail.com>
2026-08-18 15:56:41 +0400

    Update page.tsx

--- app/store/cart/page.tsx
+++ app/store/cart/page.tsx
@@ -7,21 +7,12 @@ export default async function CartPage() {
   const sb = await supabaseServer();
   const { data: auth } = await sb.auth.getUser();
 
-  let balance: number | null = null;
   let addresses: any[] = [];
   if (auth && auth.user) {
-    const { data: w } = await sb.from("wallets").select("balance_cents").maybeSingle();
-    balance = w ? (w as any).balance_cents : 0;
     const { data: a } = await sb
       .from("customer_addresses").select("*").order("is_default", { ascending: false });
     addresses = (a as any[]) || [];
   }
 
-  return (
-    <CartClient
-      balance={balance}
-      signedIn={!!(auth && auth.user)}
-      addresses={addresses}
-    />
-  );
+  return <CartClient signedIn={!!(auth && auth.user)} addresses={addresses} />;
 }
